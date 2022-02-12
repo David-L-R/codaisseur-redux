6192de5b48805793edc27d35ff132493000d6400
David Rajcher (Codaisseur) <davidrajchercodaisseur@Davids-MacBook-Pro.local>
2022-02-12 20:37:34 +0100

    feat[local alert]: alrt text for input

--- src/pages/Balance.jsx
+++ src/pages/Balance.jsx
@@ -7,7 +7,7 @@ import { selectBalance } from '../store/balance/selectors'
 import { addAlert } from '../store/alert/actions'
 
 //  COMPONENTS
-import { Page } from '../components'
+import { Alert, Page } from '../components'
 import Card from '../components/card/Card'
 
 //  STYLES
@@ -16,37 +16,37 @@ import './style/balance.css'
 const Balance = () => {
     const dispatch = useDispatch()
     const balance = useSelector(selectBalance)
-
+    const [alert, setAlert] = useState('')
     const [amount, setAmount] = useState(0)
 
     const handleInput = (e) => {
-        console.log(e.target.value)
-
-        const value = Number(e.target.value)
-
-        setAmount(value > 0 ? value : 0)
+        setAmount(Number(e.target.value))
     }
 
     const handleWithdraw = () => {
         if (balance < amount) {
-            return
+            setAlert('Cannot withdraw an amount larger than the balance')
+        } else if (amount < 0) {
+            setAlert('Cannot withdraw a negative amount')
+        } else {
+            setAlert('')
+            dispatch(withdraw(amount))
         }
-        dispatch(withdraw(amount))
+    }
+
+    const handleDeposit = () => {
+        if (amount < 0) {
+            setAlert('Cannot deposit a negative number')
+        } else {
+            dispatch(deposit(amount))
+            setAlert('')
+        }
     }
 
     const createRandomAlert = () => {
         const index = Math.floor(Math.random() * 4)
-
-        console.log('index', index)
-
         const types = ['danger', 'warning', 'success', 'info']
-
         const messages = ['error!', 'warning!', 'great!', 'some info']
-
-        console.log({
-            type: types[index],
-            message: messages[index],
-        })
 
         dispatch(
             addAlert({
@@ -62,15 +62,11 @@ const Balance = () => {
                 <p>Balance: {balance}$</p>
                 <form className="form">
                     <input type="number" onChange={handleInput} />
+                    {alert && <p className="alert-text">{alert}</p>}
                     <button type="button" onClick={handleWithdraw}>
                         Withdraw
                     </button>
-                    <button
-                        type="button"
-                        onClick={() => {
-                            dispatch(deposit(amount))
-                        }}
-                    >
+                    <button type="button" onClick={handleDeposit}>
                         Deposit
                     </button>
                     <button type="button" onClick={createRandomAlert}>
